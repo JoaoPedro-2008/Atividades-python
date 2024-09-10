5d13c7f17c942dfed3c0d10d35bb60c05fea47af
João Pedro <joaopedro1404santos@gmail.com>
2024-09-10 11:18:46 -0300

    Add files via upload

---
+++ 6-operadores-aritmeticos.py
@@ -0,0 +1,22 @@
+# Operadores Aritméticos
+# Soma
+soma = 10 + 5
+print(soma)
+# Subtração
+subtração = 10 - 5
+print(subtração)
+# Multiplicação
+multiplicacao = 10 * 5
+print(multiplicacao)
+# Divisão
+divisao = 10 / 5
+print(divisao)
+# Divisão Inteira
+divisao_inteira = 10 // 5
+print(divisao_inteira)
+# Exponenciação
+exponenciacao = 10 ** 2
+print(exponenciacao)
+# Módulo / Resto da Divisão
+modulo = 10 % 5
+print(modulo)
