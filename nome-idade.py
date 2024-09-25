93d86dbc349989acaff60f5337427b3c9b8f31ec
João Pedro <joaopedro1404santos@gmail.com>
2024-09-25 10:57:05 -0300

    Add files via upload

---
+++ nome-idade.py
@@ -0,0 +1,21 @@
+#Atribuição de variavéis
+nome= "João Pedro"
+idade= 16
+altura= 1.90
+
+#Exibir mensagem na tela
+print("Nome: ",nome, "idade: ", idade, "altura ", altura)
+#Formataçao com f-string
+print(f"Nome: {nome} idade: {idade} altura: {altura}")
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"Nome: {nome} idade: {idade} altura: {altura}")
+
+
+
+
+
+
