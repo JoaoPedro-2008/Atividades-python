ee11bdd1a4d620486b3f49f2d46a1868e3dc42c9
João Pedro <joaopedro1404santos@gmail.com>
2024-09-25 10:33:24 -0300

    Add files via upload

---
+++ 8-troca_de_valor.py
@@ -0,0 +1,11 @@
+a = 5
+b = 10
+print(f"valor de A = {a}")
+a = 5
+b = 10
+print(f"Valor de A = {a} e o valor de B = {b}")
+print("Trocando valores...")
+auxiliar = a
+a = b
+b = auxiliar
+print(f"Agora A= {a} e B= {b}")
