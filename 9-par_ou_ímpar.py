50fc6e49251cdb54e9607cff1021679530dd7aed
João Pedro <joaopedro1404santos@gmail.com>
2024-09-25 10:26:44 -0300

    Add files via upload

---
+++ 9-par_ou_ímpar.py
@@ -0,0 +1,8 @@
+# crie um algoritmo que receba um inteiro e 
+# Identifique se ele é par ou ímpar.
+
+numero = int(input("Digite um número: "))
+if (numero % 2 ==0):
+    print(f"O número {numero} é par.")
+else:
+    print(f"O número {numero} é ímpar.")
