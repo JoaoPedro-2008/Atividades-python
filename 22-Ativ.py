0d4adc1ed23e8654e5a8af14ef12888a8ff84f12
João Pedro <joaopedro1404santos@gmail.com>
2024-11-07 11:10:29 -0300

    Add files via upload

---
+++ 22-Ativ.py
@@ -0,0 +1,5 @@
+# Nome: João Pedro Marçal Santos
+# Imprime os números de 1 a 100.
+
+for numero in range(1, 101):
+    print(numero)
---
+++ 23-Ativ.py
@@ -0,0 +1,10 @@
+# Nome: joão Pedro Marçal Santos
+# Exibe a tabuada de um número fornecido pelo usuário.
+
+# Solicita ao usuário um número
+numero = int(input("Digite um número para ver sua tabuada: "))
+
+# Imprime a tabuada do número fornecido
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
